3d3fd7201f5e84a13c943960470773823306bff3
Vyassa Baratham <vbaratham@gmail.com>
2016-09-07 19:07:16 -0700

    Use different secret key env var name

--- cheese_cave/models.py
+++ cheese_cave/models.py
@@ -15,8 +15,9 @@ ISOFMT = "%Y-%m-%dT%H:%M:%S.%f"
 
 sdb = boto3.client(
     'sdb',
+    region_name='us-west-2',
     aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
 )
 
 class CheeseCave(object):
